c1289c7b9553ef84ee5fa3539b0c4a7a42b1916f
KTTimmons <89467897+KTTimmons@users.noreply.github.com>
2021-09-04 14:03:37 -0400

    Stuck In A Loop Submission

---
+++ StuckInATimeLoop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TimeLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            for (int y = 1; y <= x; y++)
+            {
+                Console.WriteLine(y +" Abracadabra");
+            }
+        }
+    }
+}
